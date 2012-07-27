5bc72ff55827e678bd5f37ac4733bd008fe1ade8
Rafael Caricio <rafael.jacinto@gmail.com>
2012-07-27 12:21:16 -0300

    Melhora o filtro de lomoize

--- custom_filters/lomoize.py
+++ custom_filters/lomoize.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from os.path import abspath
+
 from cStringIO import StringIO
 from PIL import Image
 from PIL import ImageEnhance
@@ -8,15 +10,31 @@ from PIL import ImageColor
 
 from thumbor.filters import BaseFilter, filter_method
 
+MASK_IMAGE = Image.open(abspath('./custom_filters/mask256x256.jpeg'))
 
 class Filter(BaseFilter):
 
-    @filter_method(BaseFilter.DecimalNumber, BaseFilter.DecimalNumber, BaseFilter.DecimalNumber)
-    def lomoize(self, darkness, saturation, alpha):
-        image = Image.fromstring(self.engine.get_image_mode(), 
+    @filter_method(BaseFilter.DecimalNumber, BaseFilter.DecimalNumber)
+    def lomoize(self, darkness, saturation):
+        image = Image.fromstring(self.engine.get_image_mode(),
                 self.engine.size,
                 self.engine.get_image_data())
+
+        (width, height) = image.size
+        max_size = width
+        if height > width :
+            max_size = height
+
+        mask = MASK_IMAGE.resize((max_size, max_size))
+
+        left = (max_size - width) / 2
+        upper = (max_size - height) / 2
+
+        mask = mask.crop((left, upper, left+width, upper + height))
+
         darker = ImageEnhance.Brightness(image).enhance(darkness)
         saturated = ImageEnhance.Color(image).enhance(saturation)
-        lomoized = Image.blend(saturated, darker, alpha)
+
+        lomoized = Image.composite(saturated, darker, mask)
+
         self.engine.set_image_data(lomoized.tostring())
